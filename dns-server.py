801c16fe36e7ec0e35d57304bfdef4d59a4a1af9
rsalehin <rsalehin@gmail.com>
2025-09-28 16:13:45 +0200

    feat: Stage 8 - Implement forwarding DNS server

--- dns-server.py
+++ dns-server.py
@@ -1,9 +1,10 @@
 # main.py
+import sys
 import socket
 import struct
 
 class DNSHeader:
-    def __init__(self, id, qr=1, opcode=0, aa=0, tc=0, rd=0, ra=0, z=0, rcode=0, qdcount=0, ancount=0, nscount=0, arcount=0):
+    def __init__(self, id, qr=0, opcode=0, aa=0, tc=0, rd=0, ra=0, z=0, rcode=0, qdcount=0, ancount=0, nscount=0, arcount=0):
         self.id, self.qr, self.opcode, self.aa, self.tc, self.rd, self.ra, self.z, self.rcode = id, qr, opcode, aa, tc, rd, ra, z, rcode
         self.qdcount, self.ancount, self.nscount, self.arcount = qdcount, ancount, nscount, arcount
 
@@ -29,19 +30,15 @@ def encode_domain_name(domain_name):
 def decode_name(data, offset):
     labels = []
     while True:
-        length = data[offset]
-        offset += 1
+        length = data[offset]; offset += 1
         if (length & 0b11000000) == 0b11000000:
-            pointer_byte2 = data[offset]
-            offset += 1
+            pointer_byte2 = data[offset]; offset += 1
             pointer_offset = ((length & 0b00111111) << 8) | pointer_byte2
             pointed_name, _ = decode_name(data, pointer_offset)
             labels.append(pointed_name)
             break
-        if length == 0:
-            break
-        labels.append(data[offset:offset+length].decode('utf-8'))
-        offset += length
+        if length == 0: break
+        labels.append(data[offset:offset+length].decode('utf-8')); offset += length
     return ".".join(labels), offset
 
 class DNSQuestion:
@@ -50,8 +47,7 @@ class DNSQuestion:
     @classmethod
     def unpack(cls, data, offset):
         name, offset = decode_name(data, offset)
-        type, q_class = struct.unpack('>HH', data[offset:offset+4])
-        offset += 4
+        type, q_class = struct.unpack('>HH', data[offset:offset+4]); offset += 4
         return cls(name, type, q_class), offset
     def pack(self):
         return encode_domain_name(self.name) + self.type.to_bytes(2, 'big') + self.q_class.to_bytes(2, 'big')
@@ -60,41 +56,40 @@ class DNSAnswer:
     def __init__(self, name, type, a_class, ttl, data):
         self.name, self.type, self.a_class, self.ttl, self.data = name, type, a_class, ttl, data
     def pack(self):
-        return encode_domain_name(self.name) + self.type.to_bytes(2, 'big') + self.a_class.to_bytes(2, 'big') + \
-            self.ttl.to_bytes(4, 'big') + len(self.data).to_bytes(2, 'big') + self.data
+        packed = encode_domain_name(self.name)
+        packed += struct.pack('>HHIH', self.type, self.a_class, self.ttl, len(self.data))
+        packed += self.data
+        return packed
+
+def forward_request(request_packet, resolver_address):
+    forward_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        forward_socket.sendto(request_packet, resolver_address)
+        response, _ = forward_socket.recvfrom(512)
+        return response
+    finally:
+        forward_socket.close()
 
 def main():
-    # ... main function as shown above ...
-    print("Logs from your program will appear here!")
+    resolver_address = None
+    if len(sys.argv) > 2 and sys.argv[1] == '--resolver':
+        ip, port = sys.argv[2].split(':')
+        resolver_address = (ip, int(port))
+        print(f"DNS server will forward queries to {resolver_address}")
+
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.bind(("127.0.0.1", 2053))
-    print("UDP server is listening on port 2053...")
+    print("DNS server is listening on port 2053...")
 
     while True:
         try:
             buf, source_address = udp_socket.recvfrom(512)
-            request_header = DNSHeader.unpack(buf)
-            questions = []
-            current_offset = 12
-            for _ in range(request_header.qdcount):
-                question, current_offset = DNSQuestion.unpack(buf, current_offset)
-                questions.append(question)
-
-            response_header = DNSHeader(id=request_header.id, qr=1, opcode=request_header.opcode, 
-                                        rd=request_header.rd, qdcount=len(questions), ancount=len(questions))
+            if not resolver_address:
+                print("No resolver configured. Dropping packet.")
+                continue
             
-            response_body = b""
-            for q in questions:
-                response_body += q.pack()
-            
-            for q in questions:
-                ip_bytes = socket.inet_aton("8.8.8.8")
-                answer = DNSAnswer(name=q.name, type=1, a_class=1, ttl=60, data=ip_bytes)
-                response_body += answer.pack()
-
-            response = response_header.pack() + response_body
-            udp_socket.sendto(response, source_address)
-            
+            resolver_response = forward_request(buf, resolver_address)
+            udp_socket.sendto(resolver_response, source_address)
         except Exception as e:
             print(f"An error occurred: {e}")
             break
